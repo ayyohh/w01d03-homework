fd463a86183962b11abb1752e72c8364544852c9
Alex <fuckyobitch@Alexs-MacBook-Pro.local>
2018-06-13 21:58:09 -0600

    Commit 5 - Getting to know you

--- js/app.js
+++ js/app.js
@@ -17,25 +17,45 @@
 //   }
 // }
 
-//can't find out how to write these all together
-for (let i = 1; i <= 100; i++){
-  console.log(i);
-}
+// //can't find out how to write these all together
+// for (let i = 1; i <= 100; i++){
+//   console.log(i);
+// }
+//
+// for (let i = 1; i <= 100; i++){
+//   if(i % 3 === 0){
+//     console.log("Fizz");
+//   }
+// }
+//
+// for (let i = 1; i <= 100; i++){
+//   if(i % 5 === 0){
+//     console.log("Buzz");
+//   }
+// }
+//
+// for (let i = 1; i <= 100; i++){
+//   if ((i % 3 === 0) && (i % 5 === 0)) {
+//     console.log("FizzBuzz");
+//   }
+// }
 
-for (let i = 1; i <= 100; i++){
-  if(i % 3 === 0){
-    console.log("Fizz");
-  }
-}
 
-for (let i = 1; i <= 100; i++){
-  if(i % 5 === 0){
-    console.log("Buzz");
-  }
-}
 
-for (let i = 1; i <= 100; i++){
-  if ((i % 3 === 0) && (i % 5 === 0)) {
-    console.log("FizzBuzz");
-  }
-}
+
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
+
+thom.splice(0, 1, "GameBoy");
+console.log(thom);
+
+karolin.splice(1, 1, 17);
+console.log(karolin);
+
+matt.splice(2, 1, "Gotham City");
+console.log(matt);
+
+kristyn.splice(2, 1, "Brooklyn");
+console.log(kristyn);
