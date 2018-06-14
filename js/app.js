7ebe2712947f9c87981099b9eabaec8c0c5d9d30
Alex <fuckyobitch@Alexs-MacBook-Pro.local>
2018-06-13 21:44:16 -0600

    Commit 4 - fizz buzz answered

--- js/app.js
+++ js/app.js
@@ -6,13 +6,36 @@
 // for (let i = 0; i <= 200; i += 2) {
 //   console.log(i);
 // }
+//
+// //can't get this one to work right come back to it later
+// const catSayings = ["..human...why you taking pictures of me?...", "..the catnip made me do it...", "..why does the red dot always get away.."]
+// for (let i = 0; i <= 20; i++) {
+//   console.log("Love me,pet me!");
+//   if (i % 2 === 0) {
+//       let random = catSayings[Math.floor(Math.random() * catSayings.length)];
+//       console.log(random);
+//   }
+// }
 
-//can't get this one to work right come back to it later
-const catSayings = ["..human...why you taking pictures of me?...", "..the catnip made me do it...", "..why does the red dot always get away.."]
-for (let i = 0; i <= 20; i++) {
-  console.log("Love me,pet me!");
-  if (i % 2 === 0) {
-      let random = catSayings[Math.floor(Math.random() * catSayings.length)];
-      console.log(random);
+//can't find out how to write these all together
+for (let i = 1; i <= 100; i++){
+  console.log(i);
+}
+
+for (let i = 1; i <= 100; i++){
+  if(i % 3 === 0){
+    console.log("Fizz");
+  }
+}
+
+for (let i = 1; i <= 100; i++){
+  if(i % 5 === 0){
+    console.log("Buzz");
+  }
+}
+
+for (let i = 1; i <= 100; i++){
+  if ((i % 3 === 0) && (i % 5 === 0)) {
+    console.log("FizzBuzz");
   }
 }
